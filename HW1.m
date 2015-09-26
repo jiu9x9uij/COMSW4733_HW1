349ab3dcaaaddfa4e0a68a726c5cc04424e6f655
huatong92 <ht2334@columbia.edu>
2015-09-25 21:15:15 -0400

    4th

--- HW1.m
+++ HW1.m
@@ -8,11 +8,11 @@ Initial_Distance = DistanceSensorRoomba(serPort);     % Get the Initial Distance
 Total_Distance = 0;                                                             % Initialize Total Distance
 WallSensor = WallSensorReadRoomba(serPort);
 
-while (WallSensor ~= 1)
+while 1
     display('while1');
     % Read sensors
     [BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort); % Read Bumpers
-    WallSensor = WallSensorReadRoomba(serPort);
+%     WallSensor = WallSensorReadRoomba(serPort);
     % DEBUG Print sensor values if it is triggered
     if(BumpLeft ~= 0)
         display(BumpLeft)
@@ -32,14 +32,17 @@ while (WallSensor ~= 1)
         % Right side bump into wall, turn left 45 degrees
         turnAngle(serPort, 0.1, 45);
         SetFwdVelRadiusRoomba(serPort, 0, inf);
+        break;
     elseif(BumpFront)
         % Front side bump into wall, turn left 90 degrees
         turnAngle(serPort, 0.1, 90);
-        SetFwdVelRadiusRoomba(serPort, 0, inf);         % Stop
+        SetFwdVelRadiusRoomba(serPort, 0, inf);
+        break;
     elseif(BumpLeft)
         % Left side bump into wall, turn left 135 degrees
         turnAngle(serPort, 0.1, 135);
         SetFwdVelRadiusRoomba(serPort, 0, inf);
+        break;
     else
         % Hasn't bumped into wall, keep moving forward
         SetFwdVelRadiusRoomba(serPort, 0.1, inf);
@@ -52,11 +55,36 @@ while (WallSensor ~= 1)
     
 end
 
+pos_x = 0;
+pos_y = 0;
+pos_theta = 0;
+flag = false; % if left threshold after first hit, change to true
+thr = 0.2;
+
 while 1 
     display('while2');
     % Read sensors
     [BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort); % Read Bumpers
     WallSensor = WallSensorReadRoomba(serPort);
+    d_dist = DistanceSensorRoomba(serPort);                % Poll for Distance delta
+    d_theta = AngleSensorRoomba(serPort); 
+    
+    pos_theta = pos_theta + d_theta;
+    pos_x = pos_x + sin(pos_theta) * d_dist;
+    pos_y = pos_y + cos(pos_theta) * d_dist;
+    
+    dist = sqrt(pos_x^2 + pos_y^2);
+    
+    if (dist > thr)
+        flag = true;
+    end
+    
+    if (flag && dist < thr)
+        break;
+    end
+    
+    display(pos_x);
+    display(pos_y);
     
     if(BumpLeft ~= 0)
         display(BumpLeft)
@@ -71,11 +99,14 @@ while 1
         display(WallSensor)
     end
     
-    if ~WallSensor
-        turnAngle(serPort, 0.1, -10);
-        SetFwdVelRadiusRoomba(serPort, 0, inf);
-    elseif BumpFront
+    if BumpFront
         turnAngle(serPort, 0.1, 90);
+        SetFwdVelRadiusRoomba(serPort, 0, inf);
+    elseif (BumpRight && WallSensor)
+        turnAngle(serPort, 0.1, 3);
+        SetFwdVelRadiusRoomba(serPort, 0.1, inf);
+    elseif ~WallSensor
+        turnAngle(serPort, 0.1, -15);
         SetFwdVelRadiusRoomba(serPort, 0, inf);
     else 
         SetFwdVelRadiusRoomba(serPort, 0.1, inf);
