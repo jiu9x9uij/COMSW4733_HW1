0d808b887830a58024dad67bb4291b05c4c4666f
jiu9x9uij <jiu9x9uij@gmail.com>
2015-09-29 15:13:12 -0400

    6th

--- HW1.m
+++ HW1.m
@@ -2,18 +2,17 @@ function  HW1(serPort)
 
 %% Move forward until hit wall and get ready to follow wall
 % Variable Declaration
-Total_Distance = 0;                                                             % Initialize Total Distance
+total_distance = 0;                                                             % Initialize Total Distance
 pos_x = 0;
 pos_y = 0;
 pos_theta = 0;
 flag = false;                                                                           % If left threshold range after first hit, change to true
-thr = 0.05;
+thr = 0.2;
 
 while 1
     display('while1');
     % Read sensors
     [BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort); % Read Bumpers
-%     WallSensor = WallSensorReadRoomba(serPort);
     % DEBUG Print sensor values if it is triggered
     if(BumpLeft ~= 0)
         display(BumpLeft)
@@ -24,6 +23,7 @@ while 1
     if(BumpFront)
         display(BumpFront)
     end
+    WallSensor = WallSensorReadRoomba(serPort);
     if(WallSensor ~= 0)
         display(WallSensor)
     end
@@ -49,36 +49,44 @@ while 1
         SetFwdVelRadiusRoomba(serPort, 0.3, inf);
     end
     
+     % Pause before next loop
     pause(0.1);
     
 end
 
 %% Follow the wall until back to original hit point
+d_dist = DistanceSensorRoomba(serPort);                     % Reset distance difference sensor
+d_theta = AngleSensorRoomba(serPort);                        % Reset angle difference sensor
 while 1 
     display('while2');
     % Read sensors
-    [BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort); % Read Bumpers
-    WallSensor = WallSensorReadRoomba(serPort);
-    d_dist = DistanceSensorRoomba(serPort);                % Poll for Distance delta
-    d_theta = AngleSensorRoomba(serPort); 
+    [BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);	% Read Bumpers
+    WallSensor = WallSensorReadRoomba(serPort);     % Read wall sensor
+    d_dist = DistanceSensorRoomba(serPort);                % Update distance difference
+    d_theta = AngleSensorRoomba(serPort);                   % Update angle difference
     
+    % Update location information
     pos_theta = pos_theta + d_theta;
     pos_x = pos_x + sin(pos_theta) * d_dist;
     pos_y = pos_y + cos(pos_theta) * d_dist;
     
+    % Update distance to first hit point
     dist = sqrt(pos_x^2 + pos_y^2);
     
+    % Set flag to true when leaving the threshold range after first hit
     if (dist > thr)
         flag = true;
     end
     
+    % If left threshold range after first hit and enter the threshold range
+    % again, stop robot
     if (flag && dist < thr)
         break;
     end
     
+    % DEBUG Print
     display(pos_x);
     display(pos_y);
-    
     if(BumpLeft ~= 0)
         display(BumpLeft)
     end
@@ -92,6 +100,7 @@ while 1
     display(WallSensor)
     end
     
+    % Strategy to follow the wall
     if BumpFront
         turnAngle(serPort, 0.1, 90);
         SetFwdVelRadiusRoomba(serPort, 0, inf);
@@ -104,16 +113,17 @@ while 1
     else 
         SetFwdVelRadiusRoomba(serPort, 0.2, inf);
         % Accumulate distance since first hit point
-        Total_Distance = Total_Distance + DistanceSensorRoomba(serPort); 
+        total_distance = total_distance + d_dist; 
     end
 
+    % Pause before next loop
     pause(0.1); 
    
 end
 
 %% Stop
 SetFwdVelRadiusRoomba(serPort, 0, inf);                                       
-display(Total_Distance) 
+display(total_distance) 
 display(pos_x);
 display(pos_y);
 
